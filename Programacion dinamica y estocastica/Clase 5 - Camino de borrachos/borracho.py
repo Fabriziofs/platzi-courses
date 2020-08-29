4e3b80af6516575f0bd694a7620ab52d8673480e
Fabrizio Fasanando <fabriziofasanando2@gmail.com>
2020-08-29 19:04:19 +0200

    Se agrega la visualizacion de la distancia a la que se aleja el borracho.

--- Programacion dinamica y estocastica/Clase 5 - Camino de borrachos/borracho.py
+++ Programacion dinamica y estocastica/Clase 5 - Camino de borrachos/borracho.py
@@ -14,3 +14,12 @@ class BorrachoTradicional(Borracho):
     def camina(self):
         # Elige de manera aleatoria un elemento de la lista.
         return random.choice([(0, 1), (0, -1), (1, 0), (-1, 0)])
+
+
+class BorrachoCircular(Borracho):
+    def __init__(self, nombre):
+        super().__init__(nombre)
+
+    def camina(self):
+        # arriba    abajo   derecha izquierda
+        return random.choice([(0, 2), (0, -1), (1, 0), (-2, 0)])
